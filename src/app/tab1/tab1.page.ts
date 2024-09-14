6464f12d4b5e2b0825607263e2dda4bea6e03abe
BuildTools <unconfigured@null.spigotmc.org>
2024-09-14 18:16:40 -0300

    Se agregan cosas al inicio

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,34 @@
-import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton} from '@ionic/angular/standalone';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButtons, IonMenuButton],
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButtons, IonMenuButton],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab1Page {
-  constructor() {}
+
+
+  categorias: string[] = ['Todo', 'Acción', 'Comedia', 'Drama', 'Terror', 'Romance', 'Suspenso', 'Aventura', 'Animación', 'Documental'];
+
+  selectedCategoria: string = this.categorias[0]; // Inicia la categoría en 'Todos'
+
+  // Función para verificar cual categoría seleccionó el usuario
+  selectCategory(categoria: string) {
+    this.selectedCategoria = categoria;
+    console.log("Categoría seleccionada: ", categoria);
+  }
+
+  // Verificamos que categoría está seleccionada
+  isCategorySelected(category: string) {
+    return this.selectedCategoria === category;
+  }
+
+
+  constructor() { }
 }
