25592f1969ee09d8eaaaf560f2f6b0861bc5b46a
ClaudiaSilvaG <cl.silvag@duocuc.cl>
2024-09-15 16:23:01 -0300

    se hace watchlist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,10 +9,7 @@ export const routes: Routes = [
       {
         path:'',loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
       },
-      {
-        path: 'watchlist',
-        loadComponent: () => import('./page/watchlist/watchlist.page').then( m => m.WatchlistPage)
-      },
+
       {
         path: 'series',
         loadComponent: () => import('./page/series/series.page').then( m => m.SeriesPage)
