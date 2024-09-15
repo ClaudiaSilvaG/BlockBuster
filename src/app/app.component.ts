117c4e442396cd74e7db670e84fa5ae22b016454
Bastian <ba.alvear@duoc.cl>
2024-09-15 00:43:50 -0300

    fusion bastian perfil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import {
   IonApp,
   IonRouterOutlet, MenuController
 } from '@ionic/angular/standalone';
-import {FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
 import { register } from 'swiper/element/bundle';
 
 register()
@@ -12,11 +12,12 @@ register()
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
   standalone: true,
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor(library: FaIconLibrary,private menu: MenuController) {
+  constructor(library: FaIconLibrary, private menu: MenuController) {
     // library.addIconPacks(fas)
     // library.addIconPacks(far)
     // library.addIconPacks(fad)
