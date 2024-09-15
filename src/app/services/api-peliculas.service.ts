29375a42111210e522e1371b1946c20aaa8c323d
ClaudiaSilvaG <cl.silvag@duocuc.cl>
2024-09-15 12:59:26 -0300

    SE AGREGA PARAMETRO ORDERBY PARA ORDENAR

--- src/app/services/api-peliculas.service.ts
+++ src/app/services/api-peliculas.service.ts
@@ -10,10 +10,11 @@ export class ApiPeliculasService {
     private httpClient: HttpClient
   ) { }
 
-  public getPeliculas(limite:number=20,offset:number=0){
+  public getPeliculas(limite:number=20,offset:number=0, orderBy:string=""){
     var params= new HttpParams();
     params=params.set("limit",limite.toString());
     params=params.set("offset",offset.toString());
+    params=params.set("orderBy",orderBy.toString())
     return this.httpClient.get("https://desarrollo.codemaker.cl/api/peliculas.php",{params:params});
   }
   public getPeliculasById(id:string){
