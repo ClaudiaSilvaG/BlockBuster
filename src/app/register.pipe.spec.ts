af96323736803b1ce71f821b15e8496df7045ab9
Bastian <ba.alvear@duoc.cl>
2024-09-14 22:19:26 -0300

    se elimina archivos basura register mal hecho

--- src/app/register.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RegisterPipe } from './register.pipe';
-
-describe('RegisterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RegisterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/registro.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RegistroPipe } from './registro.pipe';
-
-describe('RegistroPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RegistroPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
