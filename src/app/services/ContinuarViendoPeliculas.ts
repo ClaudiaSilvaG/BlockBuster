bb1f4e755f25a7aaf045878774ffc6ee947928c9
BuildTools <unconfigured@null.spigotmc.org>
2024-09-20 00:24:44 -0300

    Rediseña pelicula continuar viendo, entre otros detalles menores, pero importantes. Se agregan 30seg de película para continuar viendo

--- src/app/services/ContinuarViendoPeliculas.ts
+++ src/app/services/ContinuarViendoPeliculas.ts
@@ -5,4 +5,6 @@ export interface Pelicula {
   categoria: string;
   sinopsis: string;
   imagen: string;
+  url: string;
+  duration: string;
 }
--- src/app/services/api-continuarviendo.service.ts
+++ src/app/services/api-continuarviendo.service.ts
@@ -9,27 +9,33 @@ export class ContinuarViendoPeliculas {
   private peliculas: Pelicula[] = [
     {
       id: 1,
-      nombre: 'Shrek 2',
-      fecha: 2004,
-      categoria: 'Animación / Comedia',
-      sinopsis: 'Shrek y Fiona regresan con más aventuras y nuevos personajes en esta secuela llena de humor y acción. La pareja intenta adaptarse a la vida en el reino de los padres de Fiona.',
-      imagen: 'assets/imagenes/continuar_shrek.webp'
-    },
-    {
-      id: 2,
       nombre: 'Soul',
       fecha: 2020,
       categoria: 'Animación / Fantasía',
       sinopsis: 'Un profesor de música encuentra su alma en un viaje hacia el más allá, donde descubre un mundo lleno de creatividad y sabiduría. Una reflexión sobre la vida y los sueños.',
-      imagen: 'assets/imagenes/continuar_soul.webp'
+      imagen: 'assets/imagenes/continuar_soul.webp',
+      url: 'https://lightgrey-owl-901213.hostingersite.com/peliculas/soul.mp4',
+      duration: '1h 40m'
+    },
+    {
+      id: 2,
+      nombre: 'Shrek 2',
+      fecha: 2004,
+      categoria: 'Animación / Comedia',
+      sinopsis: 'Shrek y Fiona regresan con más aventuras y nuevos personajes en esta secuela llena de humor y acción. La pareja intenta adaptarse a la vida en el reino de los padres de Fiona.',
+      imagen: 'assets/imagenes/continuar_shrek.webp',
+      url: 'https://lightgrey-owl-901213.hostingersite.com/peliculas/shrek2.mp4',
+      duration: '1h 33m'
     },
     {
       id: 3,
-      nombre: 'Che Kopete',
-      fecha: 2008,
-      categoria: 'Comedia / Familiar',
-      sinopsis: 'Una comedia que sigue las desventuras de un personaje carismático y excéntrico en su vida diaria. Con situaciones cómicas y personajes entrañables.',
-      imagen: 'assets/imagenes/continuar_chekopete.webp'
+      nombre: "Intensamente 2",
+      fecha: 2023,
+      categoria: "Animación / Aventura",
+      sinopsis: "Riley, ahora adolescente, enfrenta nuevos desafíos emocionales mientras navega por la vida en la escuela secundaria. Sus emociones, lideradas por Alegría, deben adaptarse a esta nueva etapa.",
+      imagen: "assets/imagenes/continuar_intensamente2.webp",
+      url: "https://lightgrey-owl-901213.hostingersite.com/peliculas/intensamente2.mp4",
+      duration: "1h 36m"
     },
     {
       id: 4,
@@ -37,7 +43,9 @@ export class ContinuarViendoPeliculas {
       fecha: 1995,
       categoria: 'Animación / Aventura',
       sinopsis: 'Los juguetes cobran vida cuando no están siendo observados por los humanos. Sigue las peripecias de Woody y Buzz Lightyear en esta clásica película de animación.',
-      imagen: 'assets/imagenes/continuar_toystory.webp'
+      imagen: 'assets/imagenes/continuar_toystory.webp',
+      url: 'https://lightgrey-owl-901213.hostingersite.com/peliculas/toystory.mp4',
+      duration: '1h 21m'
     },
     {
       id: 5,
@@ -45,7 +53,9 @@ export class ContinuarViendoPeliculas {
       fecha: 2008,
       categoria: 'Animación / Ciencia Ficción',
       sinopsis: 'En un futuro lejano, Wall-E, un robot de limpieza, encuentra el propósito de su existencia al descubrir un nuevo mundo y luchar por un futuro mejor para la Tierra.',
-      imagen: 'assets/imagenes/continuar_walle.webp'
+      imagen: 'assets/imagenes/continuar_walle.webp',
+      url: 'https://lightgrey-owl-901213.hostingersite.com/peliculas/walle.mp4',
+      duration: '1h 38m'
     }
   ];
 
